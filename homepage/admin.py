45b4b1c5dfe2bc4f64951fd72979639165d82df7
Rungwarapon <42974200+Rungwarapon@users.noreply.github.com>
2020-03-10 18:52:47 +0700

    Add files via upload

---
+++ homepage/admin.py
@@ -0,0 +1,9 @@
+from multiprocessing.reduction import register
+
+from django.contrib import admin 
+from homepage.models import Restaurant, Faculty
+
+# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Faculty)
+
